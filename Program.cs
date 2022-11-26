74d0db324f98209b41f7b8f5ba1c3e3c1c62b4d7
Julia Ch <juliecha@yandex.ru>
2022-11-26 18:23:00 +0300

    Done Task

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ PrintArray(threeChar);
 
 string[] FillArrayString(string[] array)
 {
-    for (int i = 0; i < array.Length - 1; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         Console.WriteLine($"Введите элемент № {i + 1}");
         array[i] = Console.ReadLine();
@@ -31,7 +31,7 @@ string[] FillArrayString(string[] array)
 
 void PrintArray(string[] array)
 {
-    for (int i = 0; i < array.Length - 1; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         Console.Write($"{array[i]} ");
     }
@@ -43,7 +43,7 @@ string[] StringThreeCharFromArray(string[] array)
     string[] result = new string[array.Length];
     int length = 3;
     int count = 0;
-    for (int i = 0; i < array.Length - 1; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= length) 
         {
